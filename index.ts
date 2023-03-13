c8ac3042b70f4987a380e69025e611b43afe4c39
melekhomrani <melekhomrani87@gmail.com>
2023-03-13 11:42:33 +0100

    initial commit

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import express, {Express} from 'express';
+import express, {Express, Request, Response} from 'express';
 import cors from 'cors';
-
+import {config} from './config';
 
 const app: Express = express();
 app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running at http://localhost:${config.PORT}`);
+});
