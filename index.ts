c60a7dcf8d16d7e87c8ad94ce44bba4ffa87a071
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 22:10:29 +0100

    updated index file

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
-import {config} from './config';
+import morgan from 'morgan';
+import { config } from './config';
+import { connectToDB } from './db/db';
+import router from './routes/router';
+import errorHandler from './helpers/ErrorHandler';
 
 const app: Express = express();
+
+connectToDB();
+
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 app.use(cors());
+app.use('/api', router);
+app.use(errorHandler);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(3000, () => {
+app.listen(config.PORT, () => {
   console.log(`Server is running at http://localhost:${config.PORT}`);
 });
