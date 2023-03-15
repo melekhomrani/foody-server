2bf7e981e66d5c04b79cbc4b43682474b1f4fd97
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 16:41:26 +0100

    configured env variables

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  PORT: process.env.PORT || 3000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/foody',
-  JWT_KEY: process.env.JWT_KEY
+  PORT: process.env.PORT ,
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/foody',
+  JWT_SECRET: process.env.JWT_KEY || "3IaPNr37GjXWxhEbe+ZbOkxL66cRbaZOo40sfT0cd1BCgRQbvP9C1AOoQC0/2HSMnPsFQbytQESYRjg84HhPypOkGLnt43PNNGRpBAvPLjauRkH20mtum9QrzU+UEOgM3HzYyaqZuseou8ItEmdZGlp3+GOhYl1tHuWSucLCgUJk5eDMlFJ+xJyyjN1cb8e81+IGA/3cbcVgzcRxms7kMcsyg93QP9vS+GJiVTMEAKrkaXIV+KJ8RA0eG0Cv1djOw2cGvND2i+j+OypW/EhtzlQnWSVHRXJyU47O+T3RB1BzDxmurvlB7bpt7fN796hxJejuvs6nL7zGaaUmMrHLZQ==",
 };
