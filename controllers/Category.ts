840dbd026bfc15300805a039b0cc0889c07aa741
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 23:16:48 +0100

    updated category controller

--- controllers/Category.ts
+++ controllers/Category.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import CustomError from "../helpers/CustomError";
 import Category from "../models/Category";
 
 const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
@@ -13,6 +14,7 @@ const getAllCategories = async (req: Request, res: Response, next: NextFunction)
 const getCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await Category.findById(req.params.id);
+    if (!category) throw new CustomError(`category with id ${req.params.id} is not found`, 404);
     res.status(200).json(category);
   } catch (err: any) {
     next(err)
@@ -22,7 +24,7 @@ const getCategory = async (req: Request, res: Response, next: NextFunction) => {
 const getCategoryByName = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await Category.findOne({ name: req.params.name });
-    if (!category) throw Error(`category with name ${req.params.name} is not found`);
+    if (!category) throw new  CustomError(`category with name ${req.params.name} is not found`, 404);
     res.status(200).json(category);
   } catch (err: any) {
     next(err)
@@ -42,7 +44,7 @@ const addCategory = async (req: Request, res: Response, next: NextFunction) => {
 const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    if (!category) throw Error(`category with id ${req.params.id} is not found`);
+    if (!category) throw new CustomError(`category with id ${req.params.id} is not found`, 404);
     res.status(200).json(category);
   } catch (err: any) {
     next(err);
@@ -52,7 +54,7 @@ const updateCategory = async (req: Request, res: Response, next: NextFunction) =
 const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const category = await Category.findByIdAndDelete(req.params.id);
-    if (!category) throw Error(`category with id ${req.params.id} is not found`);
+    if (!category) throw new CustomError(`category with id ${req.params.id} is not found`, 404);
     res.status(200).json(category);
   } catch (err: any) {
     next(err);
