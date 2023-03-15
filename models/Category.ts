262d5141b69aaa382a23e42fc7eec28fc1a1cdf9
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 17:40:54 +0100

    created category model

---
+++ models/Category.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import ICategory from "../interfaces/Category";
+
+const categorySchema = new mongoose.Schema<ICategory>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    }
+  }, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+export default mongoose.model<ICategory>("category", categorySchema);
