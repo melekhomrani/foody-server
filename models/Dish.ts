18bac312742a961f50cc43d3af2c6d93ccb89b50
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 19:06:58 +0100

    created dish model

---
+++ models/Dish.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import IDishes from "../interfaces/Dish";
+
+const dishSchema = new mongoose.Schema<IDishes>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+})
+
+export default mongoose.model<IDishes>("dish", dishSchema)
