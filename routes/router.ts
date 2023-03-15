86e3d5293762fa55459ab88bb78f47204b4dfdfe
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 23:19:46 +0100

    created global route

---
+++ routes/router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import CategoryRoute from "./Category";
+import UserRoute from "./User";
+import RestaurantRoute from "./Restaurant";
+import DishRoute from "./Dish";
+
+const router = Router();
+
+router.use('/users', UserRoute);
+router.use('/categories', CategoryRoute);
+router.use('/restaurants', RestaurantRoute);
+router.use('/dishes', DishRoute);
+
+export default router;
