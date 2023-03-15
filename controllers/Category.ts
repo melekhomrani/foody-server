3b8aca626958c1c829bb11e77a685beac825d50f
melekhomrani <melekhomrani87@gmail.com>
2023-03-15 19:03:24 +0100

    created category controller

---
+++ controllers/Category.ts
@@ -0,0 +1,69 @@
+import { NextFunction, Request, Response } from "express";
+import Category from "../models/Category";
+
+const getAllCategories = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err: any) {
+    next(err);
+  }
+}
+
+const getCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+  } catch (err: any) {
+    next(err)
+  }
+}
+
+const getCategoryByName = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = await Category.findOne({ name: req.params.name });
+    if (!category) throw Error(`category with name ${req.params.name} is not found`);
+    res.status(200).json(category);
+  } catch (err: any) {
+    next(err)
+  }
+}
+
+const addCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.status(201).json(category);
+  } catch (err: any) {
+    next(err);
+  }
+}
+
+const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    if (!category) throw Error(`category with id ${req.params.id} is not found`);
+    res.status(200).json(category);
+  } catch (err: any) {
+    next(err);
+  }
+}
+
+const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) throw Error(`category with id ${req.params.id} is not found`);
+    res.status(200).json(category);
+  } catch (err: any) {
+    next(err);
+  }
+}
+
+export {
+  getAllCategories,
+  getCategory,
+  getCategoryByName,
+  addCategory,
+  updateCategory,
+  deleteCategory
+}
